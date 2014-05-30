a47beeb332fa8a2c7da8d60cdb831bd2073c1171
Jeremy <Jeremy@10.66.126.166>
2014-05-30 17:26:39 +0200

    city & day update

--- src/com/android/meteorid/dao/City.java
+++ src/com/android/meteorid/dao/City.java
@@ -4,11 +4,47 @@ import java.util.ArrayList;
 
 public class City {
 
-	// l'objet city, j'parie que tu l'avai meme pas deviné
-	
 	private int id;
 	private String name;
 	private Integer temperature;
 	private ArrayList<Day> dayList;
 	
+	public City(int id, String name, Integer temperature, ArrayList<Day> dayList) {
+		this.id = id;
+		this.name = name;
+		this.temperature = temperature;
+		this.dayList = dayList;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getTemperature() {
+		return temperature;
+	}
+
+	public void setTemperature(Integer temperature) {
+		this.temperature = temperature;
+	}
+
+	public ArrayList<Day> getDayList() {
+		return dayList;
+	}
+
+	public void setDayList(ArrayList<Day> dayList) {
+		this.dayList = dayList;
+	}
 }
--- src/com/android/meteorid/dao/Day.java
+++ src/com/android/meteorid/dao/Day.java
@@ -1,11 +1,60 @@
 package com.android.meteorid.dao;
 
+import java.sql.Timestamp;
+
 public class Day {
 	
-	private int timestamp;
-	private String name;
+	private Timestamp timestamp;
+	private int name;
 	private Integer tempMin;
 	private Integer tempMax;
 	private String icon;
+	
+	public Day(Timestamp timestamp, int name, Integer tempMin, Integer tempMax, String icon) {
+		this.timestamp = timestamp;
+		this.name = name;
+		this.tempMin = tempMin;
+		this.tempMax = tempMax;
+		this.icon = icon;
+	}
 
+	public Timestamp getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(Timestamp timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public int getName() {
+		return name;
+	}
+
+	public void setName(int name) {
+		this.name = name;
+	}
+
+	public Integer getTempMin() {
+		return tempMin;
+	}
+
+	public void setTempMin(Integer tempMin) {
+		this.tempMin = tempMin;
+	}
+
+	public Integer getTempMax() {
+		return tempMax;
+	}
+
+	public void setTempMax(Integer tempMax) {
+		this.tempMax = tempMax;
+	}
+
+	public String getIcon() {
+		return icon;
+	}
+
+	public void setIcon(String icon) {
+		this.icon = icon;
+	}
 }
