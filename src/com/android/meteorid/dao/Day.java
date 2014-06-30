33fe0c435b08453b8fe2d1c35b0c9912916ccc58
Michaos <michaos@hotmail.fr>
2014-06-30 16:32:30 +0200

    Update Day.java

--- src/com/android/meteorid/dao/Day.java
+++ src/com/android/meteorid/dao/Day.java
@@ -1,41 +1,45 @@
 package com.android.meteorid.dao;
 
-import java.sql.Timestamp;
+import org.json.JSONObject;
 
 public class Day {
 	
-	private Timestamp timestamp;
-	private String name;
+	private Integer timestamp;
 	private Integer tempDay;
 	private Integer tempMin;
 	private Integer tempMax;
 	private Integer tempMorning;
 	private Integer tempEvening;
 	private Integer tempNight;
+	private String description;
 	private String icon;
 	
-	public Day(Timestamp timestamp, String name, Integer tempMin, Integer tempMax, String icon) {
-		this.timestamp = timestamp;
-		this.name = name;
-		this.tempMin = tempMin;
-		this.tempMax = tempMax;
-		this.icon = icon;
+	public Day(JSONObject jsonObject) {
+		this.timestamp = jsonObject.optInt("timestamp");
+		this.tempDay = jsonObject.optJSONObject("temp").optInt("day");
+		this.tempMin = jsonObject.optJSONObject("temp").optInt("min");		
+		this.tempMax = jsonObject.optJSONObject("temp").optInt("max");
+		this.tempMorning = jsonObject.optJSONObject("temp").optInt("morn");
+		this.tempEvening = jsonObject.optJSONObject("temp").optInt("eve");
+		this.tempNight = jsonObject.optJSONObject("temp").optInt("night");
+		this.description = jsonObject.optJSONArray("weather").optJSONObject(0).optString("description");
+		this.icon = jsonObject.optJSONArray("weather").optJSONObject(0).optString("icon");;
 	}
 
-	public Timestamp getTimestamp() {
+	public Integer getTimestamp() {
 		return timestamp;
 	}
 
-	public void setTimestamp(Timestamp timestamp) {
+	public void setTimestamp(Integer timestamp) {
 		this.timestamp = timestamp;
 	}
 
-	public String getName() {
-		return name;
+	public Integer getTempDay() {
+		return tempDay;
 	}
 
-	public void setName(String name) {
-		this.name = name;
+	public void setTempDay(Integer tempDay) {
+		this.tempDay = tempDay;
 	}
 
 	public Integer getTempMin() {
@@ -52,6 +56,38 @@ public class Day {
 
 	public void setTempMax(Integer tempMax) {
 		this.tempMax = tempMax;
+	}
+
+	public Integer getTempMorning() {
+		return tempMorning;
+	}
+
+	public void setTempMorning(Integer tempMorning) {
+		this.tempMorning = tempMorning;
+	}
+
+	public Integer getTempEvening() {
+		return tempEvening;
+	}
+
+	public void setTempEvening(Integer tempEvening) {
+		this.tempEvening = tempEvening;
+	}
+
+	public Integer getTempNight() {
+		return tempNight;
+	}
+
+	public void setTempNight(Integer tempNight) {
+		this.tempNight = tempNight;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
 	}
 
 	public String getIcon() {
