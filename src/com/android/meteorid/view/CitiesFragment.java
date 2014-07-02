d0513188364f614769238407a0e631003c8d5e15
Michaos <michaos@hotmail.fr>
2014-07-02 11:42:24 +0200

    Update CitiesFragment.java

--- src/com/android/meteorid/view/CitiesFragment.java
+++ src/com/android/meteorid/view/CitiesFragment.java
@@ -2,7 +2,12 @@ package com.android.meteorid.view;
 
 import com.android.meteorid.R;
 import com.android.meteorid.adapter.ListCitiesAdapter;
+import com.android.meteorid.manager.DataManager;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -11,10 +16,12 @@ import android.view.ViewGroup;
 import android.widget.ListView;
 
 public class CitiesFragment extends Fragment{
-	
+
 	// ici c'est la liste des villes de l'acceuil ( tous & favoris )
-	
+
 	private static final String ARG_SECTION_NUMBER = "section_number";
+	
+	private View rootView;
 
 	public static CitiesFragment newInstance(int sectionNumber) {
 		CitiesFragment fragment = new CitiesFragment();
@@ -23,20 +30,45 @@ public class CitiesFragment extends Fragment{
 		fragment.setArguments(args);
 		return fragment;
 	}
-	
+
 	public CitiesFragment() {
 	}
+	
+	private BroadcastReceiver receiver = new BroadcastReceiver() {
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			if(getView() == null){
+				return;
+			}
+			String action = intent.getAction();
+			if(action.equals(DataManager.LOAD_BASIC_CITY_OK)){
+				System.out.println("gotcha");
+				refreshData();
+			}
+		}
+	};
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		View rootView = inflater.inflate(R.layout.fragment_main, container,	false);
+		rootView = inflater.inflate(R.layout.fragment_main, container,	false);
+
+		// la je recupere dans quel catÃ©gorie on est ( tous == 1 , favoris == 2)
 		
-		// la je recupere dans quel catégorie on est ( tous == 1 , favoris == 2)
-		getArguments().getInt(ARG_SECTION_NUMBER);
-		
-		ListView lv = (ListView) rootView.findViewById(R.id.listview);
-		lv.setAdapter(new ListCitiesAdapter(getActivity()));
+		if(getArguments().getInt(ARG_SECTION_NUMBER) == 1){
+			getActivity().registerReceiver(receiver, new IntentFilter(DataManager.LOAD_BASIC_CITY_OK));
+			DataManager myData = DataManager.getInstance(getActivity());
+			myData.launchTask();
+		}else{
+			
+		}
+
 		
 		return rootView;
+	}
+
+
+	public void refreshData(){
+		ListView lv = (ListView) rootView.findViewById(R.id.listview);
+		lv.setAdapter(new ListCitiesAdapter(getActivity(), DataManager.getInstance(getActivity()).getBasicCityList()));
 	}
 }
