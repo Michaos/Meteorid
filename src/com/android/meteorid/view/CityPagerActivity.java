6ebd10b9b00114a1928e62e0c4f4a15ca1771788
Michaos <michaos@hotmail.fr>
2014-07-06 18:14:47 +0200

    Update CityPagerActivity.java

--- src/com/android/meteorid/view/CityPagerActivity.java
+++ src/com/android/meteorid/view/CityPagerActivity.java
@@ -3,19 +3,19 @@ package com.android.meteorid.view;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.android.meteorid.R;
-import com.android.meteorid.adapter.FragmentAdapter;
-import com.android.meteorid.dao.City;
-import com.android.meteorid.manager.DataManager;
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
 
+import com.android.meteorid.R;
+import com.android.meteorid.adapter.FragmentAdapter;
+import com.android.meteorid.dao.City;
+import com.android.meteorid.manager.DataManager;
+
 public class CityPagerActivity extends FragmentActivity{
 
-	// cette activity est appeler lors du clic sur une ville dans l'écran d'acceuil
+	// cette activity est appeler lors du clic sur une ville dans l'Ã©cran d'acceuil
 
 	// Viewpager pour faire defiler les villes et leurs infos ( possede le fragment city fragment )
 
@@ -30,8 +30,8 @@ public class CityPagerActivity extends FragmentActivity{
 		}
 
 		setContentView(R.layout.activity_city);
+		
 		viewPager = (ViewPager) findViewById(R.id.pager);
-
 		List<Fragment> fragmentList = new ArrayList<Fragment>();
 		ArrayList<City> basicCityList = DataManager.getInstance(getApplicationContext()).getBasicCityList();
 
@@ -41,6 +41,7 @@ public class CityPagerActivity extends FragmentActivity{
 		
 		FragmentAdapter fa = new FragmentAdapter(getSupportFragmentManager(), fragmentList);
 		viewPager.setAdapter(fa);
+		viewPager.setCurrentItem(getIntent().getExtras().getInt("pos"));
 
 	}
 
